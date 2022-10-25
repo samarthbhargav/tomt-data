c4833851bccc37d92450dc7e50088443b3add146
samarthbhargav <samarth.bhargav92@gmail.com>
2022-10-25 09:56:01 +0200

    upd DPR with args

--- DPR/create_data_dpr.py
+++ DPR/create_data_dpr.py
@@ -1,32 +1,40 @@
 import os
 import json
 from tomt.benchmarks.gt import GTData, get_documents
-root = "/data/TOMT"
-dataset = "Movies"
 
-# save data for DPR
-file_path = "/data/TOMT/Movies/DPR/"
+import argparse
 
-documents = get_documents(os.path.join(root, dataset), hard_negatives=True, negatives=True)
+parser = argparse.ArgumentParser("create_data_dpr")
+parser.add_argument("--root", required=True)
+parser.add_argument("--dataset", required=True, choices=("Movies", "Books"))
+parser.add_argument("--file_path", required=True)
+
+args = parser.parse_args()
+
+# root = "/data/TOMT"
+# dataset = "Movies"
+#
+# # save data for DPR
+# file_path = "/data/TOMT/Movies/DPR/"
+
+documents = get_documents(os.path.join(args.root, args.dataset), hard_negatives=True, negatives=True)
 for split in ["test", "train", "validation"]:
-    gtdata = GTData(os.path.join(root, dataset, "splits", split))
-    data = gtdata.get_grouped_data(os.path.join(root, dataset), "all", hn_source="tomt_hn") #(os.path.join(root, dataset), "all")
-    #print(data[0]) # <--- one training point
+    gtdata = GTData(os.path.join(args.root, args.dataset, "splits", split))
+    data = gtdata.get_grouped_data(os.path.join(args.root, args.dataset), "all",
+                                   hn_source="tomt_hn")
 
-
-    with open(file_path+split+"_dpr.json", 'w') as f:
+    with open(args.file_path + split + "_dpr.json", 'w') as f:
         for record in data:
             neg_records = []
-            for neg_record in record["bm25_hn_negatives"]:#record["bm25_negatives"]:
-               if len(neg_record["text"].split())<1:
-                   neg_records.append({"title":neg_record["id"],"text":neg_record["title"]})
-               else:
-                   neg_records.append({"title":neg_record["id"],"text":neg_record["text"]})
+            for neg_record in record["bm25_hn_negatives"]:  # record["bm25_negatives"]:
+                if len(neg_record["text"].split()) < 1:
+                    neg_records.append({"title": neg_record["id"], "text": neg_record["title"]})
+                else:
+                    neg_records.append({"title": neg_record["id"], "text": neg_record["text"]})
             pos_records = []
             for pos_record in record["positive_documents"]:
-               pos_records.append({"title":pos_record["id"],"text":pos_record["text"]})
-            f.write(json.dumps({'_id':record["query"]["id"],'question': record["query"]["description"],
-                               'neg_paras':neg_records,'pos_paras':pos_records,
-                               'answers': [record["positive_documents"][0]["title"]]}))
+                pos_records.append({"title": pos_record["id"], "text": pos_record["text"]})
+            f.write(json.dumps({'_id': record["query"]["id"], 'question': record["query"]["description"],
+                                'neg_paras': neg_records, 'pos_paras': pos_records,
+                                'answers': [record["positive_documents"][0]["title"]]}))
             f.write('\n')
-
